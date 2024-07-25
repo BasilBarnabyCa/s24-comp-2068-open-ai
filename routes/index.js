f4839325cda4fbea955115c7208ad6dc05012f5f
Basil Barnaby <basilbarnaby.ca@gmail.com>
2024-07-25 18:37:16 -0400

    Updated prompt for standardize responses

--- routes/index.js
+++ routes/index.js
@@ -8,24 +8,6 @@ router.get("/", function (req, res, next) {
 	res.render("index", { title: `${appName}` });
 });
 
-/* POST resume for processing. */
-// router.post("/process-resume", async (req, res, next) => {
-//   const resumeText = req.body.resumeText;
-//   console.log("Resume text:", resumeText);
-
-//   try {
-//     const suggestions = await openaiService.getCareerSuggestions(resumeText);
-//     res.render("index", { title: `${appName}`, suggestions });
-//   } catch (error) {
-//     console.error(
-//       "Error details:",
-//       error.response ? error.response.data : error.message
-//     );
-// 	console.error('Error details:', error.response ? error.response.data : error.message);
-//     res.status(500).send("An error occurred while processing your request.");
-//   }
-// });
-
 router.post("/process-resume", async (req, res, next) => {
 	const { resumeText } = req.body;
 	console.log("Resume text:", resumeText);
--- services/openaiService.js
+++ services/openaiService.js
@@ -1,18 +1,27 @@
 const axios = require('axios');
 const config = require('../config/globals.js');
 
-const directive = "Based on the following resume text, suggest the best career path, highlight strong skills, and suggest new skills to learn:";
+const simpleDirective = "Based on the following resume text, suggest the best career path, highlight strong skills, and suggest new skills to learn:";
+const directive = `Based on the following resume text, generate a JSON response structured as follows:
+- summary: Provide a summary of the resume
+- careerPath: Provide an overview and detailed roles. Each role should have percentage match with the resume on a scale of 0-100. The job experience on the resume should NOT BE RETURNED and should ONLY be used to help determine the career path (THIS IS IMPORTANT). The suggested career path should show progression. careerPath should only include role, percentageMatch, and overview.
+- strongSkills: List key skills. strongSkills should categorize the skills giving a brief description of each category. strongSkills should only include category and skills.
+- suggestedLearning: Outline additional skills to learn	.
+- suggestedCertifications: Include relevant industry certifications and courses and links to these resources. suggestedCertifications should only include name, link, and description.
+- industryTrends: Describe current industry trends
+- salaryExpectations: Provide salary expectations. salaryExpectations should only the role and salary range.
+`
 
 const openaiService = {
   getCareerSuggestions: async (resumeText) => {
     try {
       const response = await axios.post('https://api.openai.com/v1/chat/completions', {
-        model: 'gpt-4o-mini', // Use the correct model as per your account access
+        model: 'gpt-4o-mini',
         messages: [
           { role: 'system', content: directive },
           { role: 'user', content: resumeText }
         ],
-        max_tokens: 150,
+        max_tokens: 1000,
       }, {
         headers: {
           'Authorization': `Bearer ${config.ApiKeys.OpenAI}`,
