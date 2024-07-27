8bb0bcac8d96e40ec65a55bd0bc4d3ec641c2b54
Basil Barnaby <basilbarnaby.ca@gmail.com>
2024-07-27 11:35:27 -0400

    Updated index page with appropriate json to html parsing, updated prompt directive, added fileserive to manage file upload

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,65 @@
 var express = require("express");
 var router = express.Router();
 var appName = "CareerCraft";
+var fileService = require("../services/fileService");
 var openaiService = require("../services/openaiService");
+const e = require("express");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
 	res.render("index", { title: `${appName}` });
 });
 
-router.post("/process-resume", async (req, res, next) => {
-	const { resumeText } = req.body;
-	console.log("Resume text:", resumeText);
+// router.post("/process-resume", async (req, res, next) => {
+// 	const { resumeText } = req.body;
+// 	console.log("Resume text:", resumeText);
+
+// 	try {
+// 		const suggestions = await openaiService.getCareerSuggestions(resumeText);
+// 		res.json({ suggestions });
+// 	} catch (error) {
+// 		console.error('Error details:', error.response ? error.response.data : error.message);
+// 		res.status(500).json({ error: "An error occurred while processing your request." });
+// 	}
+// });
+
+router.post("/process-data", fileService.uploadMiddleware, async (req, res, next) => {
+	const file = req.file;
+	let resumeText = req.body.resumeText || '';
+	let combinedText = '';
 
 	try {
-		const suggestions = await openaiService.getCareerSuggestions(resumeText);
-		res.json({ suggestions });
+		if (file) {
+			extractedResumeText = await fileService.extractText(file);
+			combinedText = extractedResumeText + ' ' + resumeText;
+		} else {
+			combinedText = resumeText;
+		}
+
+		// Process the resume text with OpenAI
+		const apiResponse = await openaiService.getCareerSuggestions(combinedText);
+		const suggestions = JSON.parse(apiResponse);
+
+		const formattedData = {
+			summary: suggestions.summary,
+			careerPath: suggestions.careerPath.map(item => ({
+			  role: item.role,
+			  percentageMatch: item.percentageMatch,
+			  overview: item.overview,
+			})),
+			strongSkills: suggestions.strongSkills,
+			suggestedLearning: suggestions.suggestedLearning,
+			suggestedCertifications: suggestions.suggestedCertifications,
+			industryTrends: suggestions.industryTrends,
+			salaryExpectations: suggestions.salaryExpectations,
+		  };
+
+		  console.log(suggestions.salaryExpectations)
+		  
+		  res.json({ formattedData });
+
 	} catch (error) {
-		console.error('Error details:', error.response ? error.response.data : error.message);
+		console.error('Error details:', error.message);
 		res.status(500).json({ error: "An error occurred while processing your request." });
 	}
 });
--- services/openaiService.js
+++ services/openaiService.js
@@ -5,11 +5,12 @@ const simpleDirective = "Based on the following resume text, suggest the best ca
 const directive = `Based on the following resume text, generate a JSON response structured as follows:
 - summary: Provide a summary of the resume
 - careerPath: Provide an overview and detailed roles. Each role should have percentage match with the resume on a scale of 0-100. The job experience on the resume should NOT BE RETURNED and should ONLY be used to help determine the career path (THIS IS IMPORTANT). The suggested career path should show progression. careerPath should only include role, percentageMatch, and overview.
-- strongSkills: List key skills. strongSkills should categorize the skills giving a brief description of each category. strongSkills should only include category and skills.
-- suggestedLearning: Outline additional skills to learn	.
+- strongSkills: List key skills. strongSkills should categorize the skills giving a brief description or summary of each category. strongSkills should only include category and skills, and summary.
+- suggestedLearning: Outline additional skills to learn	with a brief explanation as to why. suggestedLearning should only include skill and explanation and should always follow the structure: "suggestedLearning" {skill: "skill name or subject", explanation: "explanation info"}.
 - suggestedCertifications: Include relevant industry certifications and courses and links to these resources. suggestedCertifications should only include name, link, and description.
 - industryTrends: Describe current industry trends
-- salaryExpectations: Provide salary expectations. salaryExpectations should only the role and salary range.
+- salaryExpectations: Provide salary expectations for the jobs chosen in the careerPath object. This MUST BE DONE. salaryExpectations should only have the role and salary range and should always follow this structure: "salaryExpectations": {"career path role" : "min - max"}.
+Ensure the response is valid JSON without any additional text or formatting characters like \`json\` or \`text\` and non-whitespace characters. Make sure there are no unterminated strings or missing commas. Double the response to ensure there are no missing curly braces or brackets and quotes. It is important that I receive a well formatted JSON response.
 `
 
 const openaiService = {
@@ -21,7 +22,7 @@ const openaiService = {
           { role: 'system', content: directive },
           { role: 'user', content: resumeText }
         ],
-        max_tokens: 1000,
+        max_tokens: 1500,
       }, {
         headers: {
           'Authorization': `Bearer ${config.ApiKeys.OpenAI}`,
