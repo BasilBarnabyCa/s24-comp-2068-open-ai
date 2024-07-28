2273b8a3d76f67d1a0e690b9f5dc7983f62c0c53
Michael Anthony <ukomikey@gmail.com>
2024-07-27 20:32:11 -0400

    added the documentation page

--- app.js
+++ app.js
@@ -1,62 +1,72 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var config = require('./config/globals');
-var mongoose = require('mongoose');
-var cors = require('cors');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var config = require("./config/globals");
+var mongoose = require("mongoose");
+var cors = require("cors");
+var { engine } = require("express-handlebars");
 
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.engine(
+  "hbs",
+  engine({
+    extname: ".hbs",
+    defaultLayout: "layout",
+    layoutsDir: path.join(__dirname, "views"),
+    helpers: {
+      eq: (v1, v2) => v1 === v2,
+    },
+  })
+);
+app.set("view engine", "hbs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Cors
 var cors_options = {
-	origin: '*',
-	optionsSuccessStatus: 200
-}
+  origin: "*",
+  optionsSuccessStatus: 200,
+};
 app.use(cors(cors_options));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // Connect to MongoDB
 mongoose
-	.connect(config.ConnectionStrings.MongoDB, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	})
-	.then((message) => console.log("Connected Successfully!"))
-	.catch((error) => console.log(`Error while connecting: ${error}`));
-
+  .connect(config.ConnectionStrings.MongoDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected Successfully!"))
+  .catch((error) => console.log(`Error while connecting: ${error}`));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -8,89 +8,111 @@ const Suggestion = require("../models/Suggestion");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-	res.render("index", { title: `${appName}` });
+  res.render("index", { title: `${appName}` });
 });
 
-router.post("/process-data", fileService.uploadMiddleware, async (req, res, next) => {
-	const file = req.file;
-	let resumeText = req.body.resumeText || '';
-	let combinedText = '';
+router.post(
+  "/process-data",
+  fileService.uploadMiddleware,
+  async (req, res, next) => {
+    const file = req.file;
+    let resumeText = req.body.resumeText || "";
+    let combinedText = "";
 
-	try {
-		if (file) {
-			extractedResumeText = await fileService.extractText(file);
-			combinedText = extractedResumeText + ' ' + resumeText;
-		} else {
-			combinedText = resumeText;
-		}
+    try {
+      if (file) {
+        const extractedResumeText = await fileService.extractText(file);
+        combinedText = extractedResumeText + " " + resumeText;
+      } else {
+        combinedText = resumeText;
+      }
 
-		// Process the resume text with OpenAI
-		const apiResponse = await openaiService.getCareerSuggestions(combinedText);
-		const suggestions = JSON.parse(apiResponse);
-		const jsonData = JSON.stringify(suggestions);
+      // Process the resume text with OpenAI
+      const apiResponse = await openaiService.getCareerSuggestions(
+        combinedText
+      );
+      const suggestions = JSON.parse(apiResponse);
+      const jsonData = JSON.stringify(suggestions);
 
-		// Save the suggestions using the suggestion service
-		await dbService.saveSuggestion(jsonData);
+      // Save the suggestions using the suggestion service
+      await dbService.saveSuggestion(jsonData);
 
-		const formattedData = {
-			title: suggestions.title,
-			summary: suggestions.summary,
-			careerPath: suggestions.careerPath.map(item => ({
-				role: item.role,
-				percentageMatch: item.percentageMatch,
-				overview: item.overview,
-			})),
-			strongSkills: suggestions.strongSkills,
-			suggestedLearning: suggestions.suggestedLearning,
-			suggestedCertifications: suggestions.suggestedCertifications,
-			industryTrends: suggestions.industryTrends,
-			salaryExpectations: suggestions.salaryExpectations,
-		};
+      const formattedData = {
+        title: suggestions.title,
+        summary: suggestions.summary,
+        careerPath: suggestions.careerPath.map((item) => ({
+          role: item.role,
+          percentageMatch: item.percentageMatch,
+          overview: item.overview,
+        })),
+        strongSkills: suggestions.strongSkills,
+        suggestedLearning: suggestions.suggestedLearning,
+        suggestedCertifications: suggestions.suggestedCertifications,
+        industryTrends: suggestions.industryTrends,
+        salaryExpectations: suggestions.salaryExpectations,
+      };
 
-		res.json({ formattedData });
+      res.json({ formattedData });
+    } catch (error) {
+      console.error("Error details:", error.message);
+      res
+        .status(500)
+        .json({ error: "An error occurred while processing your request." });
+    }
+  }
+);
 
-	} catch (error) {
-		console.error('Error details:', error.message);
-		res.status(500).json({ error: "An error occurred while processing your request." });
-	}
+// Route for the documentation page
+router.get("/documentation", (req, res) => {
+  res.render("documentation", {
+    title: "CareerCraft Documentation",
+  });
 });
 
 // Route to fetch suggestions
 router.get("/suggestions", async (req, res) => {
-	try {
-		const suggestions = await Suggestion.find().sort({ createdAt: -1 }).limit(50);
-		res.json(suggestions);
-	} catch (error) {
-		console.error('Error fetching suggestions:', error.message);
-		res.status(500).json({ error: "An error occurred while fetching suggestions." });
-	}
+  try {
+    const suggestions = await Suggestion.find()
+      .sort({ createdAt: -1 })
+      .limit(50);
+    res.json(suggestions);
+  } catch (error) {
+    console.error("Error fetching suggestions:", error.message);
+    res
+      .status(500)
+      .json({ error: "An error occurred while fetching suggestions." });
+  }
 });
 
 // Route to fetch a single suggestion by ID
 router.get("/suggestions/:id", async (req, res, next) => {
-	const suggestionId = req.params.id;
-	try {
-		const suggestion = await dbService.getSuggestionById(suggestionId);
-		if (!suggestion) {
-			return res.status(404).json({ error: "Suggestion not found." });
-		}
-		res.json({ formattedData: JSON.parse(suggestion.data) });
-	} catch (error) {
-		console.error('Error fetching suggestion:', error.message);
-		res.status(500).json({ error: "An error occurred while fetching the suggestion." });
-	}
+  const suggestionId = req.params.id;
+  try {
+    const suggestion = await dbService.getSuggestionById(suggestionId);
+    if (!suggestion) {
+      return res.status(404).json({ error: "Suggestion not found." });
+    }
+    res.json({ formattedData: JSON.parse(suggestion.data) });
+  } catch (error) {
+    console.error("Error fetching suggestion:", error.message);
+    res
+      .status(500)
+      .json({ error: "An error occurred while fetching the suggestion." });
+  }
 });
 
 // New DELETE route for removing a suggestion
 router.delete("/suggestions/:id", async (req, res, next) => {
-    try {
-        const suggestionId = req.params.id;
-        await Suggestion.findByIdAndDelete(suggestionId);
-        res.status(204).send(); // No content, indicating successful deletion
-    } catch (error) {
-        console.error('Error deleting suggestion:', error.message);
-        res.status(500).json({ error: "An error occurred while deleting the suggestion." });
-    }
+  try {
+    const suggestionId = req.params.id;
+    await Suggestion.findByIdAndDelete(suggestionId);
+    res.status(204).send(); // No content, indicating successful deletion
+  } catch (error) {
+    console.error("Error deleting suggestion:", error.message);
+    res
+      .status(500)
+      .json({ error: "An error occurred while deleting the suggestion." });
+  }
 });
 
 module.exports = router;
